7828f1109a9727abb935daaf1f4f995fc21cc474
Luis Campos <lcamposd@symbiotic.net>
2017-12-17 17:03:29 -0600

    MVP created

--- cicom-app/components/categories/categories.controller.js
+++ cicom-app/components/categories/categories.controller.js
@@ -76,10 +76,10 @@
                       'Content-Type':'application/json',
                       'Accept':'application/json'
                     }
-      vm.newsArray;
+      vm.candidateArray;
       vm.framingArray;
-      vm.framingSelected;
-      vm.newsSelected;
+      vm.framingSelected=[];
+      vm.candidateSelected=[];
       function init(){ // función que se llama así misma para indicar que sea lo primero que se ejecute
         var myEl = angular.element( document.querySelector( '#news' ) );
         myEl.removeClass('active')
@@ -87,9 +87,9 @@
         myEl.removeClass('active')
         myEl = angular.element( document.querySelector( '#categories' ) );
         myEl.addClass('active')
-        $http.get(vm.server+'/Media/news')
+        $http.get(vm.server+'/candidates/getCandidates')
         .then(function(response, headers){
-          vm.newsArray = response.data.data;
+          vm.candidateArray = response.data.data;
         })
         $http.get(vm.server+'/subjects/getSubjects')
         .then(function(response, headers){
@@ -97,6 +97,162 @@
         })
       }init();
 
+      
+      vm.deleteFraming =function (){
+        if(vm.framingSelected != []){
+          if(!vm.framingSelected[0].subject_id){
+            alert("No se puede eliminar un tono sin guardar");
+          }else{
+            var elemId = vm.framingSelected[0].subject_id;
+            var objectToDelete = { 
+              "subject_id":elemId
+            }
+            $http.post(vm.server+'/subjects/deleteSubject', objectToDelete )
+            .then(function(response, headers){
+              if(response.data.state.code<2000){
+                $http.get(vm.server+'/subjects/getSubjects')
+                .then(function(response, headers){
+                  vm.framingArray = response.data.data;
+                  alert("Tono eliminado");
+                })
+              }else{
+                alert("Ocurrió un error al eliminar el tono, inténtelo de nuevo");
+              }
+            }) 
+          }
+        }else{
+          alert("No se ha seleccionado un tono");
+        }
+      }
+
+      vm.saveFraming =function (){
+        if(vm.framingSelected != []){
+          if(!vm.framingSelected[0].subject_id){
+            var elemName = vm.framingSelected[0].name;
+            var objectToCreate = { 
+              "name":elemName
+            }
+            //HTTP FUNCTION crear tono 
+            $http.post(vm.server+'/subjects/createSubject', objectToCreate)
+            .then(function(response, headers){
+              if(response.data.state.code<2000){
+                $http.get(vm.server+'/subjects/getSubjects')
+                .then(function(response, headers){
+                  vm.framingArray = response.data.data;
+                  alert("Tono creado");
+                  vm.framingSelected = [];
+                })
+              }else{
+                alert("Ocurrió un error al crear el tono, inténtelo de nuevo");
+              }
+            })
+          }else{
+            var elemName = vm.framingSelected[0].name;
+            var elemId = vm.framingSelected[0].subject_id;
+            var objectToCreate = { 
+              "name":elemName,
+              "subjectId":elemId
+            }
+            $http.put(vm.server+'/subjects/updateSubject', objectToCreate)
+            .then(function(response, headers){
+              if(response.data.state.code<2000){
+                $http.get(vm.server+'/subjects/getSubjects')
+                .then(function(response, headers){
+                  vm.framingArray = response.data.data;
+                  alert("Tono actualizado");
+                  vm.framingSelected = [];
+                })
+              }else{
+                alert("Ocurrió un error al actualizar el tono, inténtelo de nuevo");
+              }
+            }) 
+          }
+        }else{
+          alert("No se ha seleccionado un tono");
+        }
+      }
+
+      vm.cancelFraming = function(){
+        vm.framingSelected = [];
+      }
+
+      vm.deleteCandidate =function (){
+        if(vm.candidateSelected != []){
+          if(!vm.candidateSelected[0].candidate_id){
+            alert("No se puede eliminar un candidato sin guardar");
+          }else{
+            var elemId = vm.candidateSelected[0].candidate_id;
+            var objectToDelete = { 
+              "candidateId":elemId
+            }
+            $http.post(vm.server+'/candidates/deleteCandidate', objectToDelete )
+            .then(function(response, headers){
+              if(response.data.state.code<2000){
+                $http.get(vm.server+'/candidates/getCandidates')
+                .then(function(response, headers){
+                  vm.candidateArray = response.data.data;
+                  alert("Candidato eliminado");
+                })
+              }else{
+                alert("Ocurrió un error al eliminar el candidato, inténtelo de nuevo");
+              }
+            }) 
+          }
+        }else{
+          alert("No se ha seleccionado un candidato");
+        }
+      }
+
+      vm.saveCandidate =function (){
+        if(vm.candidateSelected != []){
+          if(!vm.candidateSelected[0].candidate_id){
+            var elemName = vm.candidateSelected[0].name;
+            var objectToCreate = { 
+              "name":elemName
+            }
+            //HTTP FUNCTION crear tono 
+            $http.post(vm.server+'/candidates/createCandidate', objectToCreate)
+            .then(function(response, headers){
+              if(response.data.state.code<2000){
+                $http.get(vm.server+'/candidates/getCandidates')
+                .then(function(response, headers){
+                  vm.candidateArray = response.data.data;
+                  alert("Candidato creado");
+                  vm.candidateSelected = [];
+                })
+              }else{
+                alert("Ocurrió un error al crear el candidato, inténtelo de nuevo");
+              }
+            })
+          }else{
+            var elemName = vm.candidateSelected[0].name;
+            var elemId = vm.candidateSelected[0].candidate_id;
+            var objectToCreate = { 
+              "name":elemName,
+              "candidateId":elemId
+            }
+            $http.put(vm.server+'/candidates/updateCandidate', objectToCreate)
+            .then(function(response, headers){
+              if(response.data.state.code<2000){
+                $http.get(vm.server+'/candidates/getCandidates')
+                .then(function(response, headers){
+                  vm.candidateArray = response.data.data;
+                  alert("Candidato actualizado");
+                  vm.candidateSelected = [];
+                })
+              }else{
+                alert("Ocurrió un error al actualizar el candidato, inténtelo de nuevo");
+              }
+            }) 
+          }
+        }else{
+          alert("No se ha seleccionado un candidate");
+        }
+      }
+
+      vm.cancelCandidate = function(){
+        vm.candidateSelected = [];
+      }
       function cloneObject(object){
         var clone = {};
         for(var key in object){
