a990719f89e7d92ecac8c019f017728a9aa2bd09
Luis Campos <lcamposd@symbiotic.net>
2018-10-19 01:07:49 -0600

    url was wrong

--- cicom-app/components/news/news.controller.js
+++ cicom-app/components/news/news.controller.js
@@ -134,11 +134,11 @@
     vm.selectedNews = false;
     vm.news;
     //LOCAL
-    vm.sqlServer = 'http://localhost:8081';
-    vm.mongoServer = 'http://localhost:8082';
+    // vm.sqlServer = 'http://localhost:8081';
+    // vm.mongoServer = 'http://localhost:8082';
     //PROD
-    // vm.sqlServer = 'http://cluster.cenat.ac.cr:8081';
-    // vm.mongoServer = 'http://cluster.cenat.ac.cr:8082';
+    vm.sqlServer = 'http://cluster.cenat.ac.cr:8081';
+    vm.mongoServer = 'http://cluster.cenat.ac.cr:8082';
     vm.newsIndex;
     vm.subjects;
     vm.mediaSelected = "Todos";
