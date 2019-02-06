44d324b395549b0f98ad5e434ae7928cfa46641e
Luna8 <anacris_28-94@hotmail.com>
2019-02-06 09:04:13 -0600

    Merge conflict solved

--- cicom-plat/dbconnection.js
+++ cicom-plat/dbconnection.js
@@ -1,12 +1,11 @@
 var mysql=require('mysql');
 //PROD
-<<<<<<< HEAD
-// var connection=mysql.createPool({
-//     host:'localhost',
-//     user:'root',
-//     password:'jicaros',
-//     database:'cicom'
-// });
+var connection=mysql.createPool({
+     host:'localhost',
+     user:'root',
+     password:'jicaros',
+     database:'cicom'
+ });
 //LOCAL LJ
 /*var connection=mysql.createPool({
     host:'localhost',
@@ -16,26 +15,11 @@ var mysql=require('mysql');
 });
 */
 //LOCAL Anita
-var connection=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'PezKoi2!',
-    database:'cicom'
-});
-module.exports=connection;
-=======
- var connection=mysql.createPool({
-     host:'localhost',
-     user:'root',
-     password:'jicaros',
-     database:'cicom'
- });
-//LOCAL
 //var connection=mysql.createPool({
 //    host:'localhost',
 //    user:'root',
-//    password:'fofirocks',
+//    password:'PezKoi2!',
 //    database:'cicom'
 //});
 module.exports=connection;
->>>>>>> 113aca9caf677c487506848e065122d8916a60cf
+
